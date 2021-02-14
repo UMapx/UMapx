a902a4b4d735bed384a2a5462b29836eee807786
asiryan <dmc5mod@yandex.ru>
2021-02-14 03:50:41 +0300

    wavelet_denoising_filter

--- sources/Wavelet/WaveletFilter.cs
+++ sources/Wavelet/WaveletFilter.cs
@@ -6,71 +6,33 @@ namespace UMapx.Wavelet
 {
     /// <summary>
     /// Defines the wavelet filter.
+    /// <remarks>
+    /// For the correct wavelet transform of a signal, it is necessary that its dimension be a power of 2.
+    /// 
+    /// </remarks>
     /// </summary>
     [Serializable]
     public class WaveletFilter : IFilter
     {
-        #region Private data
-        WaveletTransform dwt;
-        double factor = -1.0;
-        double accuracy = 0.1;
-        #endregion
-
         #region Filter components
         /// <summary>
         /// Initializes the wavelet filter.
         /// </summary>
-        /// <param name="dwt">Discrete wavelet transform</param>
+        /// <param name="waveletDecomposition">Discrete wavelet decomposition</param>
         /// <param name="factor">Factor [-1, 1]</param>
-        /// <param name="accuracy">Accuracy [0, 1]</param>
-        public WaveletFilter(WaveletTransform dwt, double factor = -1.0, double accuracy = 0.1)
+        public WaveletFilter(WaveletDecomposition waveletDecomposition, double factor = -1.0)
         {
-            this.dwt = dwt;
-            this.factor = factor;
-            this.Accuracy = accuracy;
+            WaveletDecomposition = waveletDecomposition;
+            Factor = factor;
         }
         /// <summary>
         /// Gets or sets the discrete wavelet transform.
         /// </summary>
-        public WaveletTransform DWT
-        {
-            get
-            {
-                return this.dwt;
-            }
-            set
-            {
-                this.dwt = value;
-            }
-        }
-        /// <summary>
-        /// Gets or sets the accuracy of the filter [0, 1].
-        /// </summary>
-        public double Accuracy
-        {
-            get
-            {
-                return this.accuracy;
-            }
-            set
-            {
-                this.accuracy = Maths.Double(value);
-            }
-        }
+        public WaveletDecomposition WaveletDecomposition { get; set; }
         /// <summary>
         /// Gets or sets the factor value [-1, 1].
         /// </summary>
-        public double Factor
-        {
-            get
-            {
-                return this.factor;
-            }
-            set
-            {
-                this.factor = value;
-            }
-        }
+        public double Factor { get; set; }
         #endregion
 
         #region Public apply voids
@@ -81,96 +43,87 @@ namespace UMapx.Wavelet
 
         public void Apply(double[,] data)
         {
-            // extend input
-            int r0 = data.GetLength(0), c0 = data.GetLength(1);
-            int delta = (int)(Math.Min(r0, c0) * accuracy);
-            int r = WaveletFilter.GetLength(r0 + delta * 2, dwt.Levels);
-            int c = WaveletFilter.GetLength(c0 + delta * 2, dwt.Levels);
-            double[,] extd = Matrice.Extend(data, r, c);
+            var B = WaveletDecomposition.Forward(data);
+            int n, m;
 
-            // wavelets
-            double[,] wave;
-            double alfa = 1 + factor;
-            int maxl = WaveletFilter.GetMaxLevels(Math.Min(r, c), dwt.Levels);
-            int powR = r >> maxl;
-            int powC = c >> maxl;
-            int j, k;
-
-            // forward wavelet transform
-            wave = dwt.Forward(extd);
-
-            // do job
-            for (j = 0; j < r; j++)
+            for (int i = 1; i < B.Length; i++)
             {
-                for (k = 0; k < c; k++)
+                var b = B[i];
+                n = b.GetLength(0);
+                m = b.GetLength(1);
+
+                if (!Maths.IsEven(n)) n--; // ?
+                if (!Maths.IsEven(m)) m--; // ?
+
+                // visu_shrink
+                var bb = Matrice.Reshape(b, b.Length).Abs();
+                Array.Sort(bb);
+                var median = Math.Sqrt(bb[bb.Length / 2]) * Math.Sqrt(Maths.Log2(n * m));
+
+                for (int y = 0; y < n; y++)
                 {
-                    if (j < powR && k < powC)
-                        // low-pass
-                        extd[j, k] = wave[j, k];
-                    else
-                        // high-pass filter
-                        extd[j, k] = wave[j, k] * alfa;
+                    for (int x = 0; x < m; x++)
+                    {
+                        b[y, x] = Math.Abs(b[y, x]) > median ? b[y, x] : 0;
+                    }
                 }
+
+                B[i] = b;
             }
 
-            // backward wavelet transform
-            extd = dwt.Backward(extd);
+            var output = WaveletDecomposition.Backward(B);
+            double factor = 1 + Factor;
+            n = data.GetLength(0);
+            m = data.GetLength(1);
 
-            // cutend result
-            int y0 = (r - r0) / 2;
-            int x0 = (c - c0) / 2;
-            extd = Matrice.Cut(extd, y0, x0, r0, c0);
-
-            for (j = 0; j < r0; j++)
-                for (k = 0; k < c0; k++)
-                    data[j, k] = extd[j, k];
-
-            return;
+            for (int y = 0; y < n; y++)
+            {
+                for (int x = 0; x < m; x++)
+                {
+                    // pass filtering
+                    data[y, x] = output[y, x] + factor * (data[y, x] - output[y, x]);
+                }
+            }
         }
         /// <summary>
         /// Implements a wavelet filter.
         /// </summary>
         /// <param name="data">Array</param>
+
         public void Apply(double[] data)
         {
-            // params
-            int r0 = data.GetLength(0);
-            int delta = (int)(r0 * accuracy);
-            int r = WaveletFilter.GetLength(r0 + delta * 2, dwt.Levels);
-            double[] extd = Matrice.Extend(data, r);
+            var B = WaveletDecomposition.Forward(data);
+            int n;
 
-            // wavelets
-            double[] wave;
-            double alfa = 1 + factor;
-            int maxl = WaveletFilter.GetMaxLevels(r, dwt.Levels);
-            int powR = r >> maxl;
-            int j;
-
-            // forward wavelet transform
-            wave = dwt.Forward(extd);
-
-            // do job
-            for (j = 0; j < r; j++)
+            for (int i = 1; i < B.Length; i++)
             {
-                if (j < powR)
-                    // low-pass
-                    extd[j] = wave[j];
-                else
-                    // high-pass filter
-                    extd[j] = wave[j] * alfa;
+                var b = B[i];
+                n = b.GetLength(0);
+
+                if (!Maths.IsEven(n)) n--; // ?
+
+                // visu_shrink
+                var bb = b.Abs();
+                Array.Sort(bb);
+                var median = Math.Sqrt(bb[bb.Length / 2]) * Math.Sqrt(Maths.Log2(n));
+
+                for (int y = 0; y < n; y++)
+                {
+                    b[y] = Math.Abs(b[y]) > median ? b[y] : 0;
+                }
+
+                B[i] = b;
             }
 
-            // backward wavelet transform
-            extd = dwt.Backward(extd);
+            var output = WaveletDecomposition.Backward(B);
+            double factor = 1 + Factor;
+            n = data.GetLength(0);
 
-            // cutend result
-            int y0 = (r - r0) / 2;
-            extd = Matrice.Cut(extd, y0, r0);
-
-            for (j = 0; j < r0; j++)
-                data[j] = extd[j];
-
-            return;
+            for (int y = 0; y < n; y++)
+            {
+                // pass filtering
+                data[y] = output[y] + factor * (data[y] - output[y]);
+            }
         }
         /// <summary>
         /// Implements a wavelet filter.
@@ -179,136 +132,87 @@ namespace UMapx.Wavelet
 
         public void Apply(Complex[,] data)
         {
-            // extend input
-            int r0 = data.GetLength(0), c0 = data.GetLength(1);
-            int delta = (int)(Math.Min(r0, c0) * accuracy);
-            int r = WaveletFilter.GetLength(r0 + delta * 2, dwt.Levels);
-            int c = WaveletFilter.GetLength(c0 + delta * 2, dwt.Levels);
-            Complex[,] extd = Matrice.Extend(data, r, c);
+            var B = WaveletDecomposition.Forward(data);
+            int n, m;
 
-            // wavelets
-            Complex[,] wave;
-            double alfa = 1 + factor;
-            int maxl = WaveletFilter.GetMaxLevels(Math.Min(r, c), dwt.Levels);
-            int powR = r >> maxl;
-            int powC = c >> maxl;
-            int j, k;
-
-            // forward wavelet transform
-            wave = dwt.Forward(extd);
-
-            // do job
-            for (j = 0; j < r; j++)
+            for (int i = 1; i < B.Length; i++)
             {
-                for (k = 0; k < c; k++)
+                var b = B[i];
+                n = b.GetLength(0);
+                m = b.GetLength(1);
+
+                if (!Maths.IsEven(n)) n--; // ?
+                if (!Maths.IsEven(m)) m--; // ?
+
+                // visu_shrink
+                var bb = Matrice.Reshape(b, b.Length).Abs();
+                Array.Sort(bb);
+                var median = Math.Sqrt(bb[bb.Length / 2]) * Math.Sqrt(Maths.Log2(n * m));
+
+                for (int y = 0; y < n; y++)
                 {
-                    if (j < powR && k < powC)
-                        // low-pass
-                        extd[j, k] = wave[j, k];
-                    else
-                        // high-pass filter
-                        extd[j, k] = wave[j, k] * alfa;
+                    for (int x = 0; x < m; x++)
+                    {
+                        b[y, x] = Maths.Abs(b[y, x]) > median ? b[y, x] : 0;
+                    }
                 }
+
+                B[i] = b;
             }
 
-            // backward wavelet transform
-            extd = dwt.Backward(extd);
+            var output = WaveletDecomposition.Backward(B);
+            double factor = 1 + Factor;
+            n = data.GetLength(0);
+            m = data.GetLength(1);
 
-            // cutend result
-            int y0 = (r - r0) / 2;
-            int x0 = (c - c0) / 2;
-            extd = Matrice.Cut(extd, y0, x0, r0, c0);
-
-            for (j = 0; j < r0; j++)
-                for (k = 0; k < c0; k++)
-                    data[j, k] = extd[j, k];
-
-            return;
+            for (int y = 0; y < n; y++)
+            {
+                for (int x = 0; x < m; x++)
+                {
+                    // pass filtering
+                    data[y, x] = output[y, x] + factor * (data[y, x] - output[y, x]);
+                }
+            }
         }
         /// <summary>
         /// Implements a wavelet filter.
         /// </summary>
         /// <param name="data">Array</param>
+
         public void Apply(Complex[] data)
         {
-            // params
-            int r0 = data.GetLength(0);
-            int delta = (int)(r0 * accuracy);
-            int r = WaveletFilter.GetLength(r0 + delta * 2, dwt.Levels);
-            Complex[] extd = Matrice.Extend(data, r);
+            var B = WaveletDecomposition.Forward(data);
+            int n;
 
-            // wavelets
-            Complex[] wave;
-            double alfa = 1 + factor;
-            int maxl = WaveletFilter.GetMaxLevels(r, dwt.Levels);
-            int powR = r >> maxl;
-            int j;
-
-            // forward wavelet transform
-            wave = dwt.Forward(extd);
-
-            // do job
-            for (j = 0; j < r; j++)
+            for (int i = 1; i < B.Length; i++)
             {
-                if (j < powR)
-                    // low-pass
-                    extd[j] = wave[j];
-                else
-                    // high-pass filter
-                    extd[j] = wave[j] * alfa;
-            }
+                var b = B[i];
+                n = b.GetLength(0);
 
-            // backward wavelet transform
-            extd = dwt.Backward(extd);
+                if (!Maths.IsEven(n)) n--; // ?
 
-            // cutend result
-            int y0 = (r - r0) / 2;
-            extd = Matrice.Cut(extd, y0, r0);
+                // visu_shrink
+                var bb = b.Abs();
+                Array.Sort(bb);
+                var median = Math.Sqrt(bb[bb.Length / 2]) * Math.Sqrt(Maths.Log2(n));
 
-            for (j = 0; j < r0; j++)
-                data[j] = extd[j];
-
-            return;
-        }
-        #endregion
-
-        #region Static voids
-        /// <summary>
-        /// Returns the length value for transform.
-        /// </summary>
-        /// <param name="n">Length</param>
-        /// <param name="levels">Number of levels</param>
-        /// <returns>Length</returns>
-        public static int GetLength(int n, int levels)
-        {
-            // params
-            int log2 = GetMaxLevels(n, levels);
-            int s = n, m, i;
-
-            // do job
-            for (i = 0; i < log2; i++)
-            {
-                m = Maths.Mod(s, 2);
-
-                if (s >= 2)
+                for (int y = 0; y < n; y++)
                 {
-                    if (m != 0)
-                        s = s + 1;
-                    s = s / 2;
+                    b[y] = Maths.Abs(b[y]) > median ? b[y] : 0;
                 }
+
+                B[i] = b;
             }
 
-            return s * (int)Math.Pow(2, i);
-        }
-        /// <summary>
-        /// Returns max levels of 2^K transform.
-        /// </summary>
-        /// <param name="n">Length</param>
-        /// <param name="levels">Levels</param>
-        /// <returns>New length</returns>
-        private static int GetMaxLevels(int n, int levels)
-        {
-            return (int)Math.Min(Math.Log(n, 2), levels);
+            var output = WaveletDecomposition.Backward(B);
+            double factor = 1 + Factor;
+            n = data.GetLength(0);
+
+            for (int y = 0; y < n; y++)
+            {
+                // pass filtering
+                data[y] = output[y] + factor * (data[y] - output[y]);
+            }
         }
         #endregion
     }
