4895c2be52f23cbfc8c98ee31deb66c7c320c529
asiryan <dmc5mod@yandex.ru>
2023-10-18 03:32:33 +0300

    Implement resize methods for Matrice

--- sources/Core/Matrice.cs
+++ sources/Core/Matrice.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Drawing;
 using System.Threading.Tasks;
-using System.Xml.Linq;
 
 namespace UMapx.Core
 {
@@ -6782,8 +6780,38 @@ namespace UMapx.Core
         /// <param name="input">Matrix</param>
         /// <param name="h">Height</param>
         /// <param name="w">Width</param>
+        /// <param name="interpolationMode">Interpolation mode</param>
         /// <returns>Matrix</returns>
-        public static float[,] Resize(this float[,] input, int h, int w)
+        public static float[,] Resize(this float[,] input, int h, int w, InterpolationMode interpolationMode = InterpolationMode.Bicubic)
+        {
+            if (interpolationMode == InterpolationMode.Bicubic)
+            {
+                return ResizeBicubic(input, h, w);
+            }
+            else if (interpolationMode == InterpolationMode.Bilinear)
+            {
+                return ResizeBilinear(input, h, w);
+            }
+            else if (interpolationMode == InterpolationMode.NearestNeighbor)
+            {
+                return ResizeNearestNeighbor(input, h, w);
+            }
+            else
+            {
+                throw new NotSupportedException();
+            }
+        }
+
+        #region Private methods
+
+        /// <summary>
+        /// Returns resized matrix.
+        /// </summary>
+        /// <param name="input">Matrix</param>
+        /// <param name="h">Height</param>
+        /// <param name="w">Width</param>
+        /// <returns>Matrix</returns>
+        private static float[,] ResizeBicubic(this float[,] input, int h, int w)
         {
             // get source size
             int width = input.GetLength(1);
@@ -6854,6 +6882,7 @@ namespace UMapx.Core
 
             return H;
         }
+
         /// <summary>
         /// Returns resized matrix.
         /// </summary>
@@ -6861,7 +6890,134 @@ namespace UMapx.Core
         /// <param name="h">Height</param>
         /// <param name="w">Width</param>
         /// <returns>Matrix</returns>
-        public static Complex32[,] Resize(this Complex32[,] input, int h, int w)
+        private static float[,] ResizeBilinear(this float[,] input, int h, int w)
+        {
+            // get source image size
+            int width = input.GetLength(1);
+            int height = input.GetLength(0);
+
+            float xFactor = (float)width / w;
+            float yFactor = (float)height / h;
+
+            // width and height decreased by 1
+            int ymax = height - 1;
+            int xmax = width - 1;
+
+            // output
+            float[,] H = new float[h, w];
+
+            // for each line
+            for (int y = 0; y < h; y++)
+            {
+                // Y coordinates
+                double oy = (double)y * yFactor;
+                int oy1 = (int)oy;
+                int oy2 = (oy1 == ymax) ? oy1 : oy1 + 1;
+                double dy1 = oy - (double)oy1;
+                double dy2 = 1.0 - dy1;
+
+                // for each pixel
+                for (int x = 0; x < w; x++)
+                {
+                    // X coordinates
+                    double ox = (double)x * xFactor;
+                    int ox1 = (int)ox;
+                    int ox2 = (ox1 == xmax) ? ox1 : ox1 + 1;
+                    double dx1 = ox - (double)ox1;
+                    double dx2 = 1.0 - dx1;
+
+                    // get four points
+                    var p1 = input[oy1, ox1];
+                    var p2 = input[oy1, ox2];
+                    var p3 = input[oy2, ox1];
+                    var p4 = input[oy2, ox2];
+
+                    // interpolate using 4 points
+                    H[y, x] = (float)(
+                            dy2 * (dx2 * (p1) + dx1 * (p2)) +
+                            dy1 * (dx2 * (p3) + dx1 * (p4)));
+                }
+            }
+
+            return H;
+        }
+
+        /// <summary>
+        /// Returns resized matrix.
+        /// </summary>
+        /// <param name="input">Matrix</param>
+        /// <param name="h">Height</param>
+        /// <param name="w">Width</param>
+        /// <returns>Matrix</returns>
+        private static float[,] ResizeNearestNeighbor(this float[,] input, int h, int w)
+        {
+            // get source image size
+            int width = input.GetLength(1);
+            int height = input.GetLength(0);
+
+            float xFactor = (float)width / w;
+            float yFactor = (float)height / h;
+
+            // output
+            float[,] H = new float[h, w];
+
+            // for each line
+            for (int y = 0; y < h; y++)
+            {
+                var oy = (int)(y * yFactor);
+
+                // for each pixel
+                for (int x = 0; x < w; x++)
+                {
+                    var ox = (int)(x * xFactor);
+
+                    H[y, x] = input[oy, ox];
+                }
+            }
+
+            return H;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Returns resized matrix.
+        /// </summary>
+        /// <param name="input">Matrix</param>
+        /// <param name="h">Height</param>
+        /// <param name="w">Width</param>
+        /// <param name="interpolationMode">Interpolation mode</param>
+        /// <returns>Matrix</returns>
+        public static Complex32[,] Resize(this Complex32[,] input, int h, int w, InterpolationMode interpolationMode = InterpolationMode.Bicubic)
+        {
+            if (interpolationMode == InterpolationMode.Bicubic)
+            {
+                return ResizeBicubic(input, h, w);
+            }
+            else if (interpolationMode == InterpolationMode.Bilinear)
+            {
+                return ResizeBilinear(input, h, w);
+            }
+            else if (interpolationMode == InterpolationMode.NearestNeighbor)
+            {
+                return ResizeNearestNeighbor(input, h, w);
+            }
+            else
+            {
+                throw new NotSupportedException();
+            }
+        }
+
+        #region Private methods
+
+        /// <summary>
+        /// Returns resized matrix.
+        /// </summary>
+        /// <param name="input">Matrix</param>
+        /// <param name="h">Height</param>
+        /// <param name="w">Width</param>
+        /// <returns>Matrix</returns>
+        private static Complex32[,] ResizeBicubic(this Complex32[,] input, int h, int w)
         {
             // get source size
             int width = input.GetLength(1);
@@ -6932,13 +7088,140 @@ namespace UMapx.Core
 
             return H;
         }
+
+        /// <summary>
+        /// Returns resized matrix.
+        /// </summary>
+        /// <param name="input">Matrix</param>
+        /// <param name="h">Height</param>
+        /// <param name="w">Width</param>
+        /// <returns>Matrix</returns>
+        private static Complex32[,] ResizeBilinear(this Complex32[,] input, int h, int w)
+        {
+            // get source image size
+            int width = input.GetLength(1);
+            int height = input.GetLength(0);
+
+            float xFactor = (float)width / w;
+            float yFactor = (float)height / h;
+
+            // width and height decreased by 1
+            int ymax = height - 1;
+            int xmax = width - 1;
+
+            // output
+            Complex32[,] H = new Complex32[h, w];
+
+            // for each line
+            for (int y = 0; y < h; y++)
+            {
+                // Y coordinates
+                double oy = (double)y * yFactor;
+                int oy1 = (int)oy;
+                int oy2 = (oy1 == ymax) ? oy1 : oy1 + 1;
+                double dy1 = oy - (double)oy1;
+                double dy2 = 1.0 - dy1;
+
+                // for each pixel
+                for (int x = 0; x < w; x++)
+                {
+                    // X coordinates
+                    double ox = (double)x * xFactor;
+                    int ox1 = (int)ox;
+                    int ox2 = (ox1 == xmax) ? ox1 : ox1 + 1;
+                    double dx1 = ox - (double)ox1;
+                    double dx2 = 1.0 - dx1;
+
+                    // get four points
+                    var p1 = input[oy1, ox1];
+                    var p2 = input[oy1, ox2];
+                    var p3 = input[oy2, ox1];
+                    var p4 = input[oy2, ox2];
+
+                    // interpolate using 4 points
+                    H[y, x] = (Complex32)(
+                        dy2 * (dx2 * (p1) + dx1 * (p2)) +
+                        dy1 * (dx2 * (p3) + dx1 * (p4)));
+                }
+            }
+
+            return H;
+        }
+
+        /// <summary>
+        /// Returns resized matrix.
+        /// </summary>
+        /// <param name="input">Matrix</param>
+        /// <param name="h">Height</param>
+        /// <param name="w">Width</param>
+        /// <returns>Matrix</returns>
+        private static Complex32[,] ResizeNearestNeighbor(this Complex32[,] input, int h, int w)
+        {
+            // get source image size
+            int width = input.GetLength(1);
+            int height = input.GetLength(0);
+
+            float xFactor = (float)width / w;
+            float yFactor = (float)height / h;
+
+            // output
+            Complex32[,] H = new Complex32[h, w];
+
+            // for each line
+            for (int y = 0; y < h; y++)
+            {
+                var oy = (int)(y * yFactor);
+
+                // for each pixel
+                for (int x = 0; x < w; x++)
+                {
+                    var ox = (int)(x * xFactor);
+
+                    H[y, x] = input[oy, ox];
+                }
+            }
+
+            return H;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Returns resized vector.
+        /// </summary>
+        /// <param name="input">Array</param>
+        /// <param name="h">Length</param>
+        /// <param name="interpolationMode">Interpolation mode</param>
+        /// <returns>Array</returns>
+        public static float[] Resize(this float[] input, int h, InterpolationMode interpolationMode = InterpolationMode.Bicubic)
+        {
+            if (interpolationMode == InterpolationMode.Bicubic)
+            {
+                return ResizeBicubic(input, h);
+            }
+            else if (interpolationMode == InterpolationMode.Bilinear)
+            {
+                return ResizeBilinear(input, h);
+            }
+            else if (interpolationMode == InterpolationMode.NearestNeighbor)
+            {
+                return ResizeNearestNeighbor(input, h);
+            }
+            else
+            {
+                throw new NotSupportedException();
+            }
+        }
+
+        #region Private methods
+
         /// <summary>
         /// Returns resized vector.
         /// </summary>
         /// <param name="input">Array</param>
         /// <param name="h">Length</param>
         /// <returns>Array</returns>
-        public static float[] Resize(this float[] input, int h)
+        private static float[] ResizeBicubic(this float[] input, int h)
         {
             // get source size
             int height = input.GetLength(0);
@@ -6986,13 +7269,115 @@ namespace UMapx.Core
 
             return H;
         }
+
         /// <summary>
         /// Returns resized vector.
         /// </summary>
         /// <param name="input">Array</param>
         /// <param name="h">Length</param>
         /// <returns>Array</returns>
-        public static Complex32[] Resize(this Complex32[] input, int h)
+        private static float[] ResizeBilinear(this float[] input, int h)
+        {
+            // get source image size
+            int height = input.GetLength(0);
+
+            float yFactor = (float)height / h;
+
+            // width and height decreased by 1
+            int ymax = height - 1;
+
+            // output
+            float[] H = new float[h];
+
+            // for each line
+            for (int y = 0; y < h; y++)
+            {
+                // Y coordinates
+                double oy = (double)y * yFactor;
+                int oy1 = (int)oy;
+                int oy2 = (oy1 == ymax) ? oy1 : oy1 + 1;
+                double dy1 = oy - (double)oy1;
+                double dy2 = 1.0 - dy1;
+
+                // get 2 points
+                var p1 = input[oy1];
+                var p3 = input[oy2];
+
+                // interpolate using 2 points
+                H[y] = (float)(
+                        dy2 * p1 +
+                        dy1 * p3);
+            }
+
+            return H;
+        }
+
+        /// <summary>
+        /// Returns resized vector.
+        /// </summary>
+        /// <param name="input">Array</param>
+        /// <param name="h">Length</param>
+        /// <returns>Array</returns>
+        private static float[] ResizeNearestNeighbor(this float[] input, int h)
+        {
+            // get source image size
+            int height = input.GetLength(0);
+
+            float yFactor = (float)height / h;
+
+            // output
+            float[] H = new float[h];
+
+            // for each line
+            for (int y = 0; y < h; y++)
+            {
+                var oy = (int)(y * yFactor);
+
+                // for each pixel
+                H[y] = input[oy];
+            }
+
+            return H;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Returns resized vector.
+        /// </summary>
+        /// <param name="input">Array</param>
+        /// <param name="h">Length</param>
+        /// <param name="interpolationMode">Interpolation mode</param>
+        /// <returns>Array</returns>
+        public static Complex32[] Resize(this Complex32[] input, int h, InterpolationMode interpolationMode = InterpolationMode.Bicubic)
+        {
+            if (interpolationMode == InterpolationMode.Bicubic)
+            {
+                return ResizeBicubic(input, h);
+            }
+            else if (interpolationMode == InterpolationMode.Bilinear)
+            {
+                return ResizeBilinear(input, h);
+            }
+            else if (interpolationMode == InterpolationMode.NearestNeighbor)
+            {
+                return ResizeNearestNeighbor(input, h);
+            }
+            else
+            {
+                throw new NotSupportedException();
+            }
+        }
+
+        #region Private methods
+
+        /// <summary>
+        /// Returns resized vector.
+        /// </summary>
+        /// <param name="input">Array</param>
+        /// <param name="h">Length</param>
+        /// <returns>Array</returns>
+        private static Complex32[] ResizeBicubic(this Complex32[] input, int h)
         {
             // get source size
             int height = input.GetLength(0);
@@ -7040,6 +7425,79 @@ namespace UMapx.Core
 
             return H;
         }
+
+        /// <summary>
+        /// Returns resized vector.
+        /// </summary>
+        /// <param name="input">Array</param>
+        /// <param name="h">Length</param>
+        /// <returns>Array</returns>
+        private static Complex32[] ResizeBilinear(this Complex32[] input, int h)
+        {
+            // get source image size
+            int height = input.GetLength(0);
+
+            float yFactor = (float)height / h;
+
+            // width and height decreased by 1
+            int ymax = height - 1;
+
+            // output
+            Complex32[] H = new Complex32[h];
+
+            // for each line
+            for (int y = 0; y < h; y++)
+            {
+                // Y coordinates
+                double oy = (double)y * yFactor;
+                int oy1 = (int)oy;
+                int oy2 = (oy1 == ymax) ? oy1 : oy1 + 1;
+                double dy1 = oy - (double)oy1;
+                double dy2 = 1.0 - dy1;
+
+                // get 2 points
+                var p1 = input[oy1];
+                var p3 = input[oy2];
+
+                // interpolate using 2 points
+                H[y] = (Complex32)(
+                        dy2 * p1 +
+                        dy1 * p3);
+            }
+
+            return H;
+        }
+
+        /// <summary>
+        /// Returns resized vector.
+        /// </summary>
+        /// <param name="input">Array</param>
+        /// <param name="h">Length</param>
+        /// <returns>Array</returns>
+        private static Complex32[] ResizeNearestNeighbor(this Complex32[] input, int h)
+        {
+            // get source image size
+            int height = input.GetLength(0);
+
+            float yFactor = (float)height / h;
+
+            // output
+            Complex32[] H = new Complex32[h];
+
+            // for each line
+            for (int y = 0; y < h; y++)
+            {
+                var oy = (int)(y * yFactor);
+
+                // for each pixel
+                H[y] = input[oy];
+            }
+
+            return H;
+        }
+
+        #endregion
+
         #endregion
 
         #region Shift voids
