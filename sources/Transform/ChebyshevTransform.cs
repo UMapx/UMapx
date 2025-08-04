5fa5c46c2d31019ca5e40e957de37149d3456b9f
asiryan <dmc5mod@yandex.ru>
2025-08-04 17:19:26 +0300

    Add ChebyshevTransform

--- sources/Transform/ChebyshevTransform.cs
+++ sources/Transform/ChebyshevTransform.cs
@@ -4,14 +4,14 @@ using UMapx.Core;
 namespace UMapx.Transform
 {
     /// <summary>
-    /// Defines the sine transform.
+    /// Defines the Chebyshev transform.
     /// <remarks>
     /// More information can be found on the website:
-    /// http://sernam.ru/book_prett1.php?id=91
+    /// https://en.wikipedia.org/wiki/Discrete_Chebyshev_transform
     /// </remarks>
     /// </summary>
     [Serializable]
-    public class SineTransform : ITransform
+    public class ChebyshevTransform : ITransform
     {
         #region Private data
         /// <summary>
@@ -22,10 +22,10 @@ namespace UMapx.Transform
 
         #region Initialize
         /// <summary>
-        /// Initializes the sine transform.
+        /// Initializes the Chebyshev transform.
         /// </summary>
         /// <param name="direction">Processing direction</param>
-        public SineTransform(Direction direction = Direction.Vertical)
+        public ChebyshevTransform(Direction direction = Direction.Vertical)
         {
             this.direction = direction;
         }
@@ -45,64 +45,64 @@ namespace UMapx.Transform
         }
         #endregion
 
-        #region Sine static components
+        #region Static components
         /// <summary>
-        /// Implements the construction of the sine transform matrix.
+        /// Implements the construction of the Chebyshev transform matrix.
         /// </summary>
         /// <param name="n">Size</param>
         /// <returns>Matrix</returns>
         public static float[,] Matrix(int n)
         {
-            int j, i;
-            float[,] H = new float[n, n];
-            float n1 = n + 1;
-            float scale = Maths.Sqrt(2.0f / n1);
+            float[,] T = new float[n, n];
+            float[] x = ChebyshevNodes(n);
 
-            for (i = 0; i < n; i++)
+            for (int k = 0; k < n; k++)
             {
-                for (j = 0; j < n; j++)
+                float norm = k == 0 ? (float)Math.Sqrt(1.0 / n) : (float)Math.Sqrt(2.0 / n);
+
+                for (int i = 0; i < n; i++)
                 {
-                    H[i, j] = Maths.Sin(Maths.Pi * (j + 1) * (i + 1) / n1) * scale;
+                    T[k, i] = norm * (float)Math.Cos(k * Math.Acos(x[i]));
                 }
             }
 
-            return H;
+            return T;
         }
         #endregion
 
-        #region Sine Transform
+        #region Chebyshev Transform
         /// <summary>
-        /// Forward sine transform.
+        /// Forward Chebyshev transform.
         /// </summary>
         /// <param name="A">Array</param>
         /// <returns>Array</returns>
         public float[] Forward(float[] A)
         {
             int N = A.Length;
-            float[,] U = SineTransform.Matrix(N);
+            float[,] U = ChebyshevTransform.Matrix(N);
             return Matrice.Dot(A, U);
         }
         /// <summary>
-        /// Backward sine transform.
+        /// Backward Chebyshev transform.
         /// </summary>
         /// <param name="B">Array</param>
         /// <returns>Array</returns>
         public float[] Backward(float[] B)
         {
             int N = B.Length;
-            float[,] U = SineTransform.Matrix(N);
+            float[,] U = ChebyshevTransform.Matrix(N);
             return Matrice.Dot(B, U.Transponate());
         }
         /// <summary>
-        /// Forward sine transform.
+        /// Forward Chebyshev transform.
         /// </summary>
         /// <param name="A">Matrix</param>
         /// <returns>Matrix</returns>
         public float[,] Forward(float[,] A)
         {
             int N = A.GetLength(0), M = A.GetLength(1);
-            float[,] U = SineTransform.Matrix(N);
-            float[,] V = SineTransform.Matrix(M);
+            float[,] U = ChebyshevTransform.Matrix(N);
+            float[,] V = ChebyshevTransform.Matrix(M);
 
             if (direction == Direction.Both)
             {
@@ -115,15 +115,15 @@ namespace UMapx.Transform
             return A.Dot(V.Transponate());
         }
         /// <summary>
-        /// Backward sine transform.
+        /// Backward Chebyshev transform.
         /// </summary>
         /// <param name="B">Matrix</param>
         /// <returns>Matrix</returns>
         public float[,] Backward(float[,] B)
         {
             int N = B.GetLength(0), M = B.GetLength(1);
-            float[,] U = SineTransform.Matrix(N);
-            float[,] V = SineTransform.Matrix(M);
+            float[,] U = ChebyshevTransform.Matrix(N);
+            float[,] V = ChebyshevTransform.Matrix(M);
 
             if (direction == Direction.Both)
             {
@@ -136,37 +136,37 @@ namespace UMapx.Transform
             return B.Dot(V);
         }
         /// <summary>
-        /// Forward sine transform.
+        /// Forward Chebyshev transform.
         /// </summary>
         /// <param name="A">Array</param>
         /// <returns>Array</returns>
         public Complex32[] Forward(Complex32[] A)
         {
             int N = A.Length;
-            float[,] U = SineTransform.Matrix(N);
+            float[,] U = ChebyshevTransform.Matrix(N);
             return Matrice.Dot(A, U);
         }
         /// <summary>
-        /// Backward sine transform.
+        /// Backward Chebyshev transform.
         /// </summary>
         /// <param name="B">Array</param>
         /// <returns>Array</returns>
         public Complex32[] Backward(Complex32[] B)
         {
             int N = B.Length;
-            float[,] U = SineTransform.Matrix(N);
+            float[,] U = ChebyshevTransform.Matrix(N);
             return Matrice.Dot(B, U.Transponate());
         }
         /// <summary>
-        /// Forward sine transform.
+        /// Forward Chebyshev transform.
         /// </summary>
         /// <param name="A">Matrix</param>
         /// <returns>Matrix</returns>
         public Complex32[,] Forward(Complex32[,] A)
         {
             int N = A.GetLength(0), M = A.GetLength(1);
-            float[,] U = SineTransform.Matrix(N);
-            float[,] V = SineTransform.Matrix(M);
+            float[,] U = ChebyshevTransform.Matrix(N);
+            float[,] V = ChebyshevTransform.Matrix(M);
 
             if (direction == Direction.Both)
             {
@@ -179,15 +179,15 @@ namespace UMapx.Transform
             return A.Dot(V.Transponate());
         }
         /// <summary>
-        /// Backward sine transform.
+        /// Backward Chebyshev transform.
         /// </summary>
         /// <param name="B">Matrix</param>
         /// <returns>Matrix</returns>
         public Complex32[,] Backward(Complex32[,] B)
         {
             int N = B.GetLength(0), M = B.GetLength(1);
-            float[,] U = SineTransform.Matrix(N);
-            float[,] V = SineTransform.Matrix(M);
+            float[,] U = ChebyshevTransform.Matrix(N);
+            float[,] V = ChebyshevTransform.Matrix(M);
 
             if (direction == Direction.Both)
             {
@@ -198,6 +198,25 @@ namespace UMapx.Transform
                 return U.Transponate().Dot(B);
             }
             return B.Dot(V);
+        }
+        #endregion
+
+        #region Private voids
+        /// <summary>
+        /// Returns the Chebyshev nodes of the first kind (orthogonal).
+        /// </summary>
+        /// <param name="n">Size</param>
+        /// <returns>Matrix</returns>
+        private static float[] ChebyshevNodes(int n)
+        {
+            float[] x = new float[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                x[i] = (float)Math.Cos(Math.PI * (i + 0.5) / n); // nodes on [-1, 1]
+            }
+
+            return x;
         }
         #endregion
     }
