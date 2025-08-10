30e90095588405e12f52e7cfc03811e039ccdd86
Valery Asiryan <dmc5mod@yandex.ru>
2025-08-11 00:10:00 +0300

    Add Schur decomposition

---
+++ sources/Decomposition/Schur.cs
@@ -0,0 +1,352 @@
+using System;
+using UMapx.Core;
+
+namespace UMapx.Decomposition
+{
+    /// <summary>
+    /// Defines Schur decomposition.
+    /// <remarks>
+    /// This is a representation of a square matrix in the form of a product of three matrices: A = Q * T * Q',
+    /// where Q is a unitary matrix and T is a quasi upper triangular matrix (Schur form).
+    /// More information can be found on the website:
+    /// https://en.wikipedia.org/wiki/Schur_decomposition
+    /// </remarks>
+    /// </summary>
+    [Serializable]
+    public class Schur
+    {
+        #region Private data
+        private int n;
+        private float[][] matrices; // unitary matrix
+        private float[][] hessenberg; // schur form
+        private float[] Re, Im;
+        private float eps;
+        #endregion
+
+        #region Initialize
+        /// <summary>
+        /// Initializes Schur decomposition.
+        /// </summary>
+        /// <param name="A">Square matrix</param>
+        /// <param name="eps">Epsilon [0, 1]</param>
+        public Schur(float[,] A, float eps = 1e-16f)
+        {
+            if (!Matrice.IsSquare(A))
+                throw new Exception("The matrix must be square");
+
+            this.n = A.GetLength(0);
+            this.Re = new float[n];
+            this.Im = new float[n];
+            this.eps = Maths.Float(eps);
+
+            // reduce to Hessenberg form using existing decomposition
+            var h = new Hessenberg(A);
+            this.matrices = Jagged.ToJagged(h.P); // initial unitary matrix
+            this.hessenberg = Jagged.ToJagged(h.H); // Hessenberg form
+
+            // reduce Hessenberg to real Schur form
+            hqr2();
+        }
+        #endregion
+
+        #region Standard voids
+        /// <summary>
+        /// Gets the unitary matrix Q.
+        /// </summary>
+        public float[,] Q
+        {
+            get { return Jagged.FromJagged(matrices); }
+        }
+        /// <summary>
+        /// Gets the quasi upper triangular matrix T.
+        /// </summary>
+        public float[,] T
+        {
+            get { return Jagged.FromJagged(hessenberg); }
+        }
+        #endregion
+
+        #region Private voids
+        /// <summary>
+        /// Reduces Hessenberg form to real Schur form.
+        /// </summary>
+        private void hqr2()
+        {
+            int nn = this.n;
+            int n = nn - 1;
+            int low = 0;
+            int high = nn - 1;
+            float exshift = 0;
+            float p = 0;
+            float q = 0;
+            float r = 0;
+            float s = 0;
+            float z = 0;
+            float w;
+            float x;
+            float y;
+            int i, j, k, m;
+            bool notlast;
+
+            // Store roots isolated by balanc and compute matrix norm
+            float norm = 0;
+            for (i = 0; i < nn; i++)
+            {
+                if (i < low | i > high)
+                {
+                    Re[i] = hessenberg[i][i];
+                    Im[i] = 0;
+                }
+
+                for (j = System.Math.Max(i - 1, 0); j < nn; j++)
+                    norm = norm + System.Math.Abs(hessenberg[i][j]);
+            }
+
+            // Outer loop over eigenvalue index
+            int iter = 0;
+            while (n >= low)
+            {
+                // Look for single small sub-diagonal element
+                int l = n;
+                while (l > low)
+                {
+                    s = System.Math.Abs(hessenberg[l - 1][l - 1]) + System.Math.Abs(hessenberg[l][l]);
+                    if (s == 0)
+                        s = norm;
+                    if (System.Math.Abs(hessenberg[l][l - 1]) < eps * s)
+                        break;
+                    l--;
+                }
+
+                // Check for convergence
+                if (l == n)
+                {
+                    // One root found
+                    hessenberg[n][n] = hessenberg[n][n] + exshift;
+                    Re[n] = hessenberg[n][n];
+                    Im[n] = 0;
+                    n--;
+                    iter = 0;
+                }
+                else if (l == n - 1)
+                {
+                    // Two roots found
+                    w = hessenberg[n][n - 1] * hessenberg[n - 1][n];
+                    p = (hessenberg[n - 1][n - 1] - hessenberg[n][n]) / 2;
+                    q = p * p + w;
+                    z = (float)System.Math.Sqrt(System.Math.Abs(q));
+                    hessenberg[n][n] = hessenberg[n][n] + exshift;
+                    hessenberg[n - 1][n - 1] = hessenberg[n - 1][n - 1] + exshift;
+                    x = hessenberg[n][n];
+
+                    if (q >= 0)
+                    {
+                        // Real pair
+                        z = (p >= 0) ? (p + z) : (p - z);
+                        Re[n - 1] = x + z;
+                        Re[n] = Re[n - 1];
+                        if (z != 0)
+                            Re[n] = x - w / z;
+                        Im[n - 1] = 0;
+                        Im[n] = 0;
+                        x = hessenberg[n][n - 1];
+                        s = System.Math.Abs(x) + System.Math.Abs(z);
+                        p = x / s;
+                        q = z / s;
+                        r = (float)System.Math.Sqrt(p * p + q * q);
+                        p = p / r;
+                        q = q / r;
+
+                        // Row modification
+                        for (j = n - 1; j < nn; j++)
+                        {
+                            z = hessenberg[n - 1][j];
+                            hessenberg[n - 1][j] = q * z + p * hessenberg[n][j];
+                            hessenberg[n][j] = q * hessenberg[n][j] - p * z;
+                        }
+
+                        // Column modification
+                        for (i = 0; i <= n; i++)
+                        {
+                            z = hessenberg[i][n - 1];
+                            hessenberg[i][n - 1] = q * z + p * hessenberg[i][n];
+                            hessenberg[i][n] = q * hessenberg[i][n] - p * z;
+                        }
+
+                        // Accumulate transformations
+                        for (i = 0; i < nn; i++)
+                        {
+                            z = matrices[i][n - 1];
+                            matrices[i][n - 1] = q * z + p * matrices[i][n];
+                            matrices[i][n] = q * matrices[i][n] - p * z;
+                        }
+                    }
+                    else
+                    {
+                        // Complex pair
+                        Re[n - 1] = x + p;
+                        Re[n] = x + p;
+                        Im[n - 1] = z;
+                        Im[n] = -z;
+                    }
+                    n = n - 2;
+                    iter = 0;
+                }
+                else
+                {
+                    // No convergence yet
+                    x = hessenberg[n][n];
+                    y = 0;
+                    w = 0;
+                    if (l < n)
+                    {
+                        y = hessenberg[n - 1][n - 1];
+                        w = hessenberg[n][n - 1] * hessenberg[n - 1][n];
+                    }
+
+                    if (iter == 10)
+                    {
+                        exshift += x;
+                        for (i = low; i <= n; i++)
+                            hessenberg[i][i] -= x;
+                        s = System.Math.Abs(hessenberg[n][n - 1]) + System.Math.Abs(hessenberg[n - 1][n - 2]);
+                        x = y = 0.75f * s;
+                        w = -0.4375f * s * s;
+                    }
+
+                    if (iter == 30)
+                    {
+                        s = (y - x) / 2;
+                        s = s * s + w;
+                        if (s > 0)
+                        {
+                            s = (float)System.Math.Sqrt(s);
+                            if (y < x)
+                                s = -s;
+                            s = x - w / ((y - x) / 2 + s);
+                            for (i = low; i <= n; i++)
+                                hessenberg[i][i] -= s;
+                            exshift += s;
+                            x = y = w = 0.964f;
+                        }
+                    }
+
+                    iter = iter + 1; // (Could check iteration count here.)
+
+                    // Look for two consecutive small sub-diagonal elements
+                    m = n - 2;
+                    while (m >= l)
+                    {
+                        z = hessenberg[m][m];
+                        r = x - z;
+                        s = y - z;
+                        p = (r * s - w) / hessenberg[m + 1][m] + hessenberg[m][m + 1];
+                        q = hessenberg[m + 1][m + 1] - z - r - s;
+                        r = hessenberg[m + 2][m + 1];
+                        s = System.Math.Abs(p) + System.Math.Abs(q) + System.Math.Abs(r);
+                        p = p / s;
+                        q = q / s;
+                        r = r / s;
+                        if (m == l)
+                            break;
+                        if (System.Math.Abs(hessenberg[m][m - 1]) * (System.Math.Abs(q) + System.Math.Abs(r)) <
+                            eps * (System.Math.Abs(p) * (System.Math.Abs(hessenberg[m - 1][m - 1]) + System.Math.Abs(z) + System.Math.Abs(hessenberg[m + 1][m + 1]))))
+                            break;
+                        m--;
+                    }
+
+                    for (i = m + 2; i <= n; i++)
+                    {
+                        hessenberg[i][i - 2] = 0;
+                        if (i > m + 2)
+                            hessenberg[i][i - 3] = 0;
+                    }
+
+                    // float QR step involving rows l:n and columns m:n
+                    for (k = m; k <= n - 1; k++)
+                    {
+                        notlast = (k != n - 1);
+                        if (k != m)
+                        {
+                            p = hessenberg[k][k - 1];
+                            q = hessenberg[k + 1][k - 1];
+                            r = (notlast ? hessenberg[k + 2][k - 1] : 0);
+                            x = System.Math.Abs(p) + System.Math.Abs(q) + System.Math.Abs(r);
+                            if (x != 0)
+                            {
+                                p = p / x;
+                                q = q / x;
+                                r = r / x;
+                            }
+                        }
+
+                        if (x == 0)
+                            break;
+
+                        s = (float)System.Math.Sqrt(p * p + q * q + r * r);
+                        if (p < 0)
+                            s = -s;
+
+                        if (s != 0)
+                        {
+                            if (k != m)
+                                hessenberg[k][k - 1] = -s * x;
+                            else if (l != m)
+                                hessenberg[k][k - 1] = -hessenberg[k][k - 1];
+
+                            p = p + s;
+                            x = p / s;
+                            y = q / s;
+                            z = r / s;
+                            q = q / p;
+                            r = r / p;
+
+                            // Row modification
+                            for (j = k; j < nn; j++)
+                            {
+                                p = hessenberg[k][j] + q * hessenberg[k + 1][j];
+                                if (notlast)
+                                {
+                                    p = p + r * hessenberg[k + 2][j];
+                                    hessenberg[k + 2][j] = hessenberg[k + 2][j] - p * z;
+                                }
+
+                                hessenberg[k][j] = hessenberg[k][j] - p * x;
+                                hessenberg[k + 1][j] = hessenberg[k + 1][j] - p * y;
+                            }
+
+                            // Column modification
+                            for (i = 0; i <= System.Math.Min(n, k + 3); i++)
+                            {
+                                p = x * hessenberg[i][k] + y * hessenberg[i][k + 1];
+                                if (notlast)
+                                {
+                                    p = p + z * hessenberg[i][k + 2];
+                                    hessenberg[i][k + 2] = hessenberg[i][k + 2] - p * r;
+                                }
+
+                                hessenberg[i][k] = hessenberg[i][k] - p;
+                                hessenberg[i][k + 1] = hessenberg[i][k + 1] - p * q;
+                            }
+
+                            // Accumulate transformations
+                            for (i = 0; i < nn; i++)
+                            {
+                                p = x * matrices[i][k] + y * matrices[i][k + 1];
+                                if (notlast)
+                                {
+                                    p = p + z * matrices[i][k + 2];
+                                    matrices[i][k + 2] = matrices[i][k + 2] - p * r;
+                                }
+
+                                matrices[i][k] = matrices[i][k] - p;
+                                matrices[i][k + 1] = matrices[i][k + 1] - p * q;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        #endregion
+    }
+}
