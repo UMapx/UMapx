6570641620ad7c17ce0e4ae452a4d82e3f0e8c10
asiryan <dmc5mod@yandex.ru>
2025-09-10 18:27:33 +0300

    Fixture for FastCosineTransform

--- sources/Transform/FastCosineTransform.cs
+++ sources/Transform/FastCosineTransform.cs
@@ -40,30 +40,58 @@ namespace UMapx.Transform
         /// <returns>Array</returns>
         public override float[] Forward(float[] A)
         {
-            int N = A.Length, N2 = N / 2, i, k;
-            Complex32[] B = new Complex32[N];
+            int N = A.Length;
 
-            for (i = 0; i < N2; i++)
+            if ((N & 1) == 0)
             {
-                var j = 2 * i;
-                B[i     ] = A[j];
-                B[i + N2] = A[N - j - 1];
+                int N2 = N / 2, i, k;
+                Complex32[] B = new Complex32[N];
+
+                for (i = 0; i < N2; i++)
+                {
+                    var j = 2 * i;
+                    B[i] = A[j];
+                    B[i + N2] = A[N - j - 1];
+                }
+
+                B = FFT.Forward(B);
+
+                float[] C = new float[N];
+
+                Complex32 c = -Maths.I * Maths.Pi / (2f * N);
+
+                for (k = 0; k < N; k++)
+                {
+                    C[k] = 2.0f * (B[k] * Maths.Exp(c * k)).Real / Maths.Sqrt(2f * N);
+                }
+
+                C[0] /= Maths.Sqrt2;
+                return C;
             }
-
-            B = FFT.Forward(B);
-
-            float[] C = new float[N];
-
-            Complex32 c = -Maths.I * Maths.Pi / (2f * N);
-
-            for (k = 0; k < N; k++)
+            else
             {
-                C[k] = 2.0f * (B[k] * Maths.Exp(c * k)).Real / Maths.Sqrt(2f * N);
+                int N2 = 2 * N;
+                Complex32[] B = new Complex32[N2];
+
+                for (int n = 0; n < N; n++)
+                {
+                    B[n] = A[n];
+                    B[N2 - 1 - n] = A[n];
+                }
+
+                var Y = FFT.Forward(B);
+
+                float[] C = new float[N];
+                Complex32 phase = -Maths.I * Maths.Pi / (2f * N);
+
+                for (int k = 0; k < N; k++)
+                {
+                    C[k] = (Y[k] * Maths.Exp(phase * k)).Real / Maths.Sqrt(2f * N);
+                }
+
+                C[0] /= Maths.Sqrt2;
+                return C;
             }
-
-            C[0] = C[0] / Maths.Sqrt2;
-
-            return C;
         }
         /// <summary>
         /// Backward transform.
@@ -72,29 +100,65 @@ namespace UMapx.Transform
         /// <returns>Array</returns>
         public override float[] Backward(float[] B)
         {
-            int N = B.Length, N2 = N / 2, i, k;
-            Complex32[] A = new Complex32[N];
-            Complex32 c = Maths.I * Maths.Pi / (2f * N);
+            int N = B.Length;
 
-            for (k = 0; k < N; k++)
+            if ((N & 1) == 0)
             {
-                A[k] = B[k] * Maths.Exp(c * k) * Maths.Sqrt(2f * N);
+                int N2 = N / 2, i, k;
+                Complex32[] A = new Complex32[N];
+                Complex32 c = Maths.I * Maths.Pi / (2f * N);
+
+                for (k = 0; k < N; k++)
+                {
+                    A[k] = B[k] * Maths.Exp(c * k) * Maths.Sqrt(2f * N);
+                }
+
+                A[0] /= Maths.Sqrt(2f);
+
+                A = FFT.Backward(A);
+
+                float[] C = new float[N];
+
+                for (i = 0; i < N2; i++)
+                {
+                    var j = 2 * i;
+                    C[j] = A[i].Real / N;
+                    C[j + 1] = A[N - i - 1].Real / N;
+                }
+
+                return C;
             }
-
-            A[0] /= Maths.Sqrt(2f);
-
-            A = FFT.Backward(A);
-
-            float[] C = new float[N];
-
-            for (i = 0; i < N2; i++)
+            else
             {
-                var j = 2 * i;
-                C[j    ] = A[i        ].Real / N;
-                C[j + 1] = A[N - i - 1].Real / N;
-            }
+                int N2 = 2 * N;
+                Complex32[] A = new Complex32[N2];
+                Complex32 phase = Maths.I * Maths.Pi / (2f * N);
 
-            return C;
+                for (int k = 0; k < N; k++)
+                {
+                    Complex32 Vk = B[k] * Maths.Exp(phase * k) * Maths.Sqrt(2f * N);
+                    if (k == 0) Vk *= Maths.Sqrt2;
+                    A[k] = Vk;
+                }
+
+                for (int k = 1; k < N; k++)
+                {
+                    A[N2 - k] = A[k].Conjugate;
+                }
+
+                A[N] = 0;
+
+                var C = FFT.Backward(A);
+
+                float[] x = new float[N];
+
+                for (int n = 0; n < N; n++)
+                {
+                    x[n] = C[n].Real / (2f * N);
+                }
+
+                return x;
+            }
         }
         #endregion
     }
