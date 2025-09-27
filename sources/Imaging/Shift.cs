92af6cba746e07e8a6d555a375348985084575a5
asiryan <dmc5mod@yandex.ru>
2025-09-27 21:40:32 +0300

    Refactoring

--- sources/Imaging/Shift.cs
+++ sources/Imaging/Shift.cs
@@ -73,34 +73,52 @@ namespace UMapx.Imaging
         /// </summary>
         /// <param name="bmData">Bitmap data</param>
         /// <param name="bmSrc">Bitmap data</param>
-        public void Apply(BitmapData bmData, BitmapData bmSrc)
+        public unsafe void Apply(BitmapData bmData, BitmapData bmSrc)
         {
             if (bmData.PixelFormat != PixelFormat.Format32bppArgb || bmSrc.PixelFormat != PixelFormat.Format32bppArgb)
                 throw new NotSupportedException("Only support Format32bppArgb pixelFormat");
 
-            // image properties:
             int width = bmSrc.Width;
             int height = bmSrc.Height;
-            int stride = bmSrc.Stride;
-
-            // exception!
-            if (bmData.Width != width ||
-                bmData.Height != height)
+            if (bmData.Width != width || bmData.Height != height)
                 throw new ArgumentException("Input image sizes must be the same");
 
-            // applying only for X:
-            if (x != 0 && y == 0)
-                ShiftX(bmData, bmSrc, width, height, stride);
+            int dx = ((X % width) + width) % width;
+            int dy = ((Y % height) + height) % height;
+            if (dx == 0 && dy == 0) return;
 
-            // applying only for Y:
-            else if (y != 0 && x == 0)
-                ShiftY(bmData, bmSrc, width, height, stride);
+            int srcStride = bmSrc.Stride;
+            int dstStride = bmData.Stride;
 
-            // applying for two sides:
-            else
+            byte* pSrc = (byte*)bmSrc.Scan0.ToPointer();
+            byte* pDst = (byte*)bmData.Scan0.ToPointer();
+
+            int rowBytes = checked(width * 4);
+            if (rowBytes > srcStride || rowBytes > dstStride)
+                throw new ArgumentException("Row size exceeds stride");
+
+            byte* tempRow = stackalloc byte[rowBytes];
+
+            for (int j = 0; j < height; j++)
             {
-                ShiftX(bmSrc, bmData, width, height, stride);
-                ShiftY(bmData, bmSrc, width, height, stride);
+                int sy = j - dy; if (sy < 0) sy += height;
+
+                byte* srcRow = pSrc + (long)srcStride * sy;
+                byte* dstRow = pDst + (long)dstStride * j;
+
+                if (dx == 0)
+                {
+                    Buffer.MemoryCopy(srcRow, dstRow, dstStride, rowBytes);
+                }
+                else
+                {
+                    int splitBytes = dx * 4;
+                    int rightBytes = rowBytes - splitBytes;
+
+                    Buffer.MemoryCopy(srcRow + splitBytes, tempRow, rowBytes, rightBytes);
+                    Buffer.MemoryCopy(srcRow, tempRow + rightBytes, rowBytes - rightBytes, splitBytes);
+                    Buffer.MemoryCopy(tempRow, dstRow, dstStride, rowBytes);
+                }
             }
         }
         /// <summary>
@@ -137,97 +155,6 @@ namespace UMapx.Imaging
             var Src = (Bitmap)Data.Clone();
             Apply(Data, Src);
             Src.Dispose();
-        }
-        #endregion
-
-        #region Private voids
-        /// <summary>
-        /// Apply filter.
-        /// </summary>
-        /// <param name="bmData">Bitmap data</param>
-        /// <param name="bmSrc">Bitmap data</param>
-        /// <param name="width">Image width</param>
-        /// <param name="height">Image height</param>
-        /// <param name="stride">Stride</param>
-        private unsafe void ShiftY(BitmapData bmData, BitmapData bmSrc, int width, int height, int stride)
-        {
-            byte* p = (byte*)bmData.Scan0.ToPointer();
-            byte* pSrc = (byte*)(void*)bmSrc.Scan0.ToPointer();
-            int sX = 0, sY = 0, front = 0, reverse = 0, f = 1;
-            int i, j;
-
-            for (j = 0; j < height; j++)
-            {
-                sY = j + y; // Offset of axis Y
-
-                for (i = 0; i < width; i++, p += 4)
-                {
-                    sX = i; // Offset of axis X
-
-                    if (sY < height && sY >= 0)
-                    {
-                        front = Math.Abs(sX * 4 + sY * stride);
-                        p[0] = pSrc[front];
-                        p[1] = pSrc[front + 1];
-                        p[2] = pSrc[front + 2];
-                        p[3] = pSrc[front + 3];
-                    }
-                    else
-                    {
-                        f = (sY < 0) ? -1 : 1;
-
-                        reverse = Math.Abs(sX * 4 + (sY - f * height) * stride);
-                        p[0] = pSrc[reverse];
-                        p[1] = pSrc[reverse + 1];
-                        p[2] = pSrc[reverse + 2];
-                        p[3] = pSrc[reverse + 3];
-                    }
-                }
-            }
-        }
-        /// <summary>
-        /// Apply filter.
-        /// </summary>
-        /// <param name="bmData">Bitmap data</param>
-        /// <param name="bmSrc">Bitmap data</param>
-        /// <param name="width">Image width</param>
-        /// <param name="height">Image height</param>
-        /// <param name="stride">Stride</param>
-        private unsafe void ShiftX(BitmapData bmData, BitmapData bmSrc, int width, int height, int stride)
-        {
-            byte* p = (byte*)bmData.Scan0.ToPointer();
-            byte* pSrc = (byte*)(void*)bmSrc.Scan0.ToPointer();
-            int sX = 0, sY = 0, front = 0, reverse = 0, f = 1;
-            int i, j;
-
-            for (j = 0; j < height; j++)
-            {
-                sY = j; // Offset of axis Y
-
-                for (i = 0; i < width; i++, p += 4)
-                {
-                    sX = i + x; // Offset of axis X
-
-                    if (sX < width && sX >= 0)
-                    {
-                        front = Math.Abs(sX * 4 + sY * stride);
-                        p[0] = pSrc[front];
-                        p[1] = pSrc[front + 1];
-                        p[2] = pSrc[front + 2];
-                        p[3] = pSrc[front + 3];
-                    }
-                    else
-                    {
-                        f = (sX < 0) ? -1 : 1;
-
-                        reverse = Math.Abs((sX - f * width) * 4 + sY * stride);
-                        p[0] = pSrc[reverse];
-                        p[1] = pSrc[reverse + 1];
-                        p[2] = pSrc[reverse + 2];
-                        p[3] = pSrc[reverse + 3];
-                    }
-                }
-            }
         }
         #endregion
     }
