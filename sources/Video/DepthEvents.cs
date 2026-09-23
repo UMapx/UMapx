49a6f834f375c14b269a4ecd4f8f52c97ab0f3f0
asiryan <dmc5mod@yandex.ru>
2026-09-23 19:21:47 +0300

    Remove video to another lib

--- sources/Video/DepthEvents.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UMapx.Video
-{
-    /// <summary>
-    /// New depth event handler.
-    /// </summary>
-    /// <param name="sender">Sender.</param>
-    /// <param name="eventArgs">Event arguments.</param>
-    public delegate void NewDepthEventHandler(object sender, NewDepthEventArgs eventArgs);
-}
